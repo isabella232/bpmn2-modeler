ae1f17782ef3fcc08a34185013b7b81a26e9d3b1
Paul Leacu <pleacu@redhat.com>
2018-04-18 09:44:05 -0400

    [533540] Don't limit wide subprocess diagram to 1000
    Change-Id: I6809897fe1bef6f8e8b356feacb474fe8d1b137b
    Signed-off-by: Paul Leacu <pleacu@redhat.com>

--- plugins/org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/core/features/activity/AbstractAddActivityFeature.java
+++ plugins/org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/core/features/activity/AbstractAddActivityFeature.java
@@ -72,8 +72,8 @@ public abstract class AbstractAddActivityFeature<T extends Activity>
 		int height = getHeight(context);
 		
 		GraphicsAlgorithm targetAlgorithm = context.getTargetContainer().getGraphicsAlgorithm();
-		
-		if (targetAlgorithm != null) {
+
+		if (targetAlgorithm != null && !(targetAlgorithm instanceof Rectangle)) {
 			width = Math.min(targetAlgorithm.getWidth(), width);
 			height = Math.min(targetAlgorithm.getHeight(), height);
 		}
